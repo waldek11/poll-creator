f94f27c1d707b2c844bda42b361e1dc5098b883c
Follownet <wdziubek11@gmail.com>
2024-04-28 12:50:32 -0500

    Adjust font sizes

--- src/typography.ts
+++ src/typography.ts
@@ -6,24 +6,24 @@ const T = styled.span`
 
 export const T1 = styled(T)`
   font-weight: 600;
-  size: 1.25rem; // 20px
+  font-size: 1.25rem; // 20px
   line-height: 2rem; //24px
 `;
 
 export const T2 = styled(T)`
   font-weight: 500;
-  size: 1rem; // 16px
+  font-size: 1rem; // 16px
   line-height: 1.25rem; //20px
 `;
 
 export const T3 = styled(T)`
   font-weight: 600;
-  size: 0.75rem; // 12px
+  font-size: 0.75rem; // 12px
   line-height: 0.875rem;
 `;
 
 export const T4 = styled(T)`
   font-weight: 500;
-  size: 0.6875rem; // 11px
+  font-size: 0.6875rem; // 11px
   line-height: 0.875rem;
 `;
